f46b473bf7bf2018eae788757bab8a1fad68f7b0
Marija Stamatovic <marijastamatovic92@gmail.com>
2016-12-04 01:22:39 +0100

    jooj

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ weatherApp.controller('forecastControler',['$scope', '$resource', '$routeParams'
 	$scope.city=cityService.city;
 	$scope.days=$routeParams.days || '2';
 
-	$scope.weatherApi=$resource("//api.openweathermap.org/data/2.5/forecast/daily",{callback: "JSON_CALLBACK"}, {get:{method:"JSONP"}});
+	$scope.weatherApi=$resource("https://api.openweathermap.org/data/2.5/forecast/daily",{callback: "JSON_CALLBACK"}, {get:{method:"JSONP"}});
 	$scope.weatherResult=$scope.weatherApi.get({q:$scope.city, cnt:$scope.days, APPID:'aec5946887b0b1060c15bd80d606c62d'});
 
 	$scope.convertToCel=function(degK){
