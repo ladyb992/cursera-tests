8fa43785267c1ff59c9d96a2b0207229d6a8aa30
Marija Stamatovic <marijastamatovic92@gmail.com>
2016-12-10 15:05:50 +0100

    greske

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('ngApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2'
+  'ngApp.view1',
+  'ngApp.view2',
+  'ngApp.facebook'
   
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+config(['$routeProvider', function($routeProvider) {
+ 
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/facebook'});
 }]);
