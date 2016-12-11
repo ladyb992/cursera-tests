c26ee099a188f75b13edeb7a475573fd9343dfb8
Marija Stamatovic <marijastamatovic92@gmail.com>
2016-12-11 17:18:19 +0100

    aaa

--- app/facebook/facebook.js
+++ app/facebook/facebook.js
@@ -42,11 +42,11 @@ $scope.logout = function(){
   });
 }
 function refresh(){
-$facebook.api("/me?fields=id,email,first_name,last_name,gender,link").then(function(response){
+$facebook.api("/me?fields=id,name,email,first_name,last_name,gender,link").then(function(response){
 $scope.welcomeMsg = "Welcome " + response.name;
 $scope.isLoggedIn = true;
 $scope.userInfo = response;
-$facebook.api('/me/picture').then(function(response){
+$facebook.api('/me?fields=picture,data').then(function(response){
 $scope.picture = response.data.url;
 });
 
